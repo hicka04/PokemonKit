c38ef29cb0bd95b851010515f52b890e73afc588
hicka04 <hicka04@gmail.com>
2020-10-04 23:02:19 +0900

    タイプの型追加

---
+++ Sources/PokemonKit/Pokemon.swift
@@ -0,0 +1,12 @@
+//
+//  Pokemon.swift
+//  
+//
+//  Created by hicka04 on 2020/10/04.
+//
+
+import Foundation
+
+public struct Pokemon {
+    
+}
---
+++ Sources/PokemonKit/Type.swift
@@ -0,0 +1,49 @@
+//
+//  Type.swift
+//  
+//
+//  Created by hicka04 on 2020/10/04.
+//
+
+import Foundation
+
+extension Pokemon {
+    public enum `Type` {
+        /// ノーマル
+        case normal
+        /// ほのお
+        case fire
+        /// みず
+        case water
+        /// でんき
+        case electric
+        /// くさ
+        case grass
+        /// こおり
+        case ice
+        /// かくとう
+        case fighting
+        /// どく
+        case poison
+        /// じめん
+        case ground
+        /// ひこう
+        case flying
+        /// エスパー
+        case psychic
+        /// むし
+        case bug
+        /// いわ
+        case rock
+        /// ゴースト
+        case ghost
+        /// ドラゴン
+        case dragon
+        /// あく
+        case dark
+        /// はがね
+        case steel
+        /// フェアリー
+        case fairy
+    }
+}
