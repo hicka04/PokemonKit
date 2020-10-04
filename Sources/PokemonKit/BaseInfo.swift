4e4dcc22b3027cbf1ae93849cdd25f9d132f3aa5
hicka04 <hicka04@gmail.com>
2020-10-04 23:08:00 +0900

    図鑑番号を表すプロパティ追加

--- Sources/PokemonKit/BaseInfo.swift
+++ Sources/PokemonKit/BaseInfo.swift
@@ -9,8 +9,19 @@ import Foundation
 
 extension Pokemon {
     public struct BaseInfo {
+        public let number: Number
         public let name: String
         public let types: (Type, Type?)
         public let baseStats: BaseStats
+    }
+}
+
+extension Pokemon.BaseInfo {
+    public struct Number: RawRepresentable {
+        public let rawValue: UInt16
+        
+        public init(rawValue: UInt16) {
+            self.rawValue = rawValue
+        }
     }
 }
